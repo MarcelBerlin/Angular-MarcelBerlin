d9365b08411374964e21c882d3456041aaf8fa86
Marcel Berlin <berlin_m@gmx.de>
2023-02-25 15:21:36 +0100

    added a new component called slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,26 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['coding.jpg', 'computer.jpg', 'create.jpg', 'programming.jpg'];
-  currentImage = 0;
-  showImg = true;
-
-  ngOnInit() {
-    this.updateImg();
-  }
-
-  updateImg() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImg = false;
-
-      setTimeout(() => {
-        this.showImg = true;
-      }, 10);
-    }, 8000);
-  }
+export class AppComponent {
 
 
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['coding.jpg',
+    'computer.jpg',
+    'create.jpg',
+    'programming.jpg'];
+
+  headlines = [
+    'Push your Coding Experience to the next level!',
+    'Improve your clean coding skills',
+    'Create new Worlds',
+    'Home-Office makes you more producive'];
+
+  currentImage = 0;
+  showImg = true;
+
+  ngOnInit() {
+    this.updateImg();
+  }
+
+  updateImg() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImg = false;
+
+      setTimeout(() => {
+        this.showImg = true;
+      }, 5);
+    }, 8000);
+  }
+
+}
