6f0af0e7f10e50253b871a753f92691e1065ffb3
Marcel Berlin <berlin_m@gmx.de>
2023-02-25 14:20:01 +0100

    added images and added slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'marcelberlin.de';
+export class AppComponent implements OnInit {
+  images = ['coding.jpg', 'computer.jpg', 'create.jpg', 'programming.jpg'];
+  currentImage = 0;
+  showImg = true;
+
+  ngOnInit() {
+    this.updateImg();
+  }
+
+  updateImg() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImg = false;
+
+      setTimeout(() => {
+        this.showImg = true;
+      }, 10);
+    }, 8000);
+  }
+
+
 }
