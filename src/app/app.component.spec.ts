c7044dd93d5b49e5bfb458d41d960f85a55017eb
Marcel Berlin <berlin_m@gmx.de>
2023-03-02 11:42:52 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'marcelberlin.de'`, () => {
+  it(`should have as title 'marcelberlin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('marcelberlin.de');
+    expect(app.title).toEqual('marcelberlin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('marcelberlin.de app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('marcelberlin app is running!');
   });
 });
